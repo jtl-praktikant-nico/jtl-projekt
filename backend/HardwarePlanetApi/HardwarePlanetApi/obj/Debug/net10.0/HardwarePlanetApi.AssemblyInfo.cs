472b9461a6777aeda4d202c30439cbb9578febd2
jtl-praktikant-nico <jtlpraktikant3@outlook.de>
2026-06-29 13:59:29 +0200

    react

--- backend/HardwarePlanetApi/HardwarePlanetApi/obj/Debug/net10.0/HardwarePlanetApi.AssemblyInfo.cs
+++ backend/HardwarePlanetApi/HardwarePlanetApi/obj/Debug/net10.0/HardwarePlanetApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HardwarePlanetApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df05d26b378ea4c58f4d1360fcc16b06f41ab72d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b8ea238124388100b9152bbf8cf840351868e0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HardwarePlanetApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HardwarePlanetApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+useEffect(() => {
+    fetch('http://localhost:5102/api/products')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+      .catch((error) => {
+        setError(error.message)
+        setLoading(false)
+      })
+  }, [])
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
